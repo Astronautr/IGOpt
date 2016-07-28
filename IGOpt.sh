d6391cda9c26be6ecfbc07564b467d92c22c1f6b
Astronaut <astronautr@gmail.com>
2016-07-28 20:50:47 +0600

    del some trash lines from script

--- IGOpt.sh
+++ IGOpt.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-# if [[ $# -gt 0 ]]; then
-	
-# 	files=$(find . ~ -maxdepth 1 -name $1)
-# 	files=${files//"./"}
-# 	if [[ "$files" == "$1" ]]; then
-# 		echo "input file found"
-# 	else
-# 		echo "error: input file not found"
-# 	fi
-
-# else
-# 	echo "Conditions will be grab from input"
-# fi
-
-# if [[ "$str1" == "$str2" ]]; then
-# 	echo "equals"
-# fi
 javac -classpath ./lib/p1788-rev444.jar -sourcepath ./src -d ./out ./src/Main.java
 java -classpath ./lib/p1788-rev444.jar:./out Main $@
